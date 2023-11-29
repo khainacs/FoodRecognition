2b44b8a4645d84978e2296ce3832b9cbfcf0ee54
khainacs <khai.nguyenanh03@gmail.com>
2023-11-29 14:26:44 +0700

    first commit

---
+++ Javis/src/Java/src/test/java/com/example/foodrecognition/FoodRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodrecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
