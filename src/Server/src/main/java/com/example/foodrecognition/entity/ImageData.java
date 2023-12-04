7c9bb8a86ef7761ac8dc2b1fd100ec3f4dfdb37f
khainacs <khai.nguyenanh03@gmail.com>
2023-12-04 12:30:25 +0700

    "fix enviroment"

--- src/Server/src/main/java/com/example/foodrecognition/entity/ImageData.java
+++ src/Server/src/main/java/com/example/foodrecognition/entity/ImageData.java
@@ -1,10 +1,11 @@
 package com.example.foodrecognition.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 /**
  * ImageData is table in db: foodrecognition
